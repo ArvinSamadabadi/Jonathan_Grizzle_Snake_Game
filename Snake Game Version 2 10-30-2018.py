8cd58ea303d20d7c044cf0dc88e0319e5e67db5d
jgrizz13 <44587555+jgrizz13@users.noreply.github.com>
2018-11-10 13:57:44 -0500

    Update Snake Game Version 2 10-30-2018.py
    Changed the size of the snake. I realized that img variable was the snake, so I renamed it to snake.

--- Snake Game Version 2 10-30-2018.py
+++ Snake Game Version 2 10-30-2018.py
@@ -27,7 +27,7 @@ def collide(x1, x2, y1, y2, w1, w2, h1, h2):
 	else:return False
 def die(screen, score):
 	f=pygame.font.SysFont('Times New Roman', 30);t=f.render('Your amazing score was: '+str(score) + "!!!!", True, (255, 255, 255));screen.blit(t, (10, 270));pygame.display.update();pygame.time.wait(2000);sys.exit(0)
-xs = [290, 290, 290, 290, 290];ys = [290, 270, 250, 230, 210];dirs = 0;score = 0;applepos = (random.randint(0, 590), random.randint(0, 590));pygame.init();s=pygame.display.set_mode((600, 600));pygame.display.set_caption('Snake');appleimage = pygame.Surface((25, 25));appleimage.fill((0, 0, 238));img = pygame.Surface((40, 20));img.fill((255,255,0));f = pygame.font.SysFont('Times New Roman', 20);clock = pygame.time.Clock()
+xs = [290, 290, 290, 290, 290];ys = [290, 270, 250, 230, 210];dirs = 0;score = 0;applepos = (random.randint(0, 590), random.randint(0, 590));pygame.init();s=pygame.display.set_mode((600, 600));pygame.display.set_caption('Snake');appleimage = pygame.Surface((25, 25));appleimage.fill((0, 0, 238));Snake = pygame.Surface((40, 20));Snake.fill((255,255,0));f = pygame.font.SysFont('Times New Roman', 20);clock = pygame.time.Clock()
 while True:
 	clock.tick(10)
 	for e in pygame.event.get():
@@ -53,7 +53,7 @@ while True:
 	elif dirs==3:xs[0] -= 20	
 	s.fill((0,0,0))	
 	for i in range(0, len(xs)):
-		s.blit(img, (xs[i], ys[i]))
+		s.blit(Snake, (xs[i], ys[i]))
 	s.blit(appleimage, applepos);t=f.render(str(score), True, (255, 255, 0));s.blit(t, (10, 10));pygame.display.update()
 					
 					
