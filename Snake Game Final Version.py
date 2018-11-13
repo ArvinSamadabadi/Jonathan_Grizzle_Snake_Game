9b4c4345b1ca64919068248bfec58b2262ab0207
jgrizz13 <44587555+jgrizz13@users.noreply.github.com>
2018-11-13 11:29:20 -0500

    Exit
    Exit

--- Snake Game Final Version.py
+++ Snake Game Final Version.py
@@ -22,14 +22,16 @@ while (timer != 0 ):
 
       
 def collide(x1, x2, y1, y2, w1, w2, h1, h2):
-	if x1+w1>x2 and x1<x2+w2 and y1+h1>y2 and y1<y2+h2:return True
-	else:return False
+	if x1+w1>x2 and x1<x2+w2 and y1+h1>y2 and y1<y2+h2:
+		return True
+	else:
+		return False
 def die(screen, score):
 	f=pygame.font.SysFont('Times New Roman', 30);t=f.render('Your amazing score was: '+str(score) + "!!!!", True, (255, 255, 255))
 	winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
 	screen.blit(t, (10, 270))
 	pygame.display.update()
-	pygame.time.wait(2000);sys.exit(0)
+	pygame.time.wait(2000)
 xs = [290, 290, 290, 290, 290];ys = [290, 270, 250, 230, 210];dirs = 0;score = 0;applepos = (random.randint(0, 590), random.randint(0, 590))
 pygame.init();s=pygame.display.set_mode((600, 600));pygame.display.set_caption('Snake')
 Food = pygame.Surface((20,20));Food.fill((0, 0, 238))
@@ -47,10 +49,13 @@ while True:
 			elif e.key == K_RIGHT and dirs != 3:dirs = 1
 	i = len(xs)-1
 	while i >= 2:
-		if collide(xs[0], xs[i], ys[0], ys[i], 20, 20, 20, 20):die(s, score)
+		if collide(xs[0], xs[i], ys[0], ys[i], 20, 20, 20, 20):
+			die(s, score)
+			break
 		i-= 1
-	if collide(xs[0], applepos[0], ys[0], applepos[1], 40, 20, 40, 20):score+=1;xs.append(700);ys.append(700);applepos=(random.randint(0,590),random.randint(0,590))
-	if xs[0] < 0 or xs[0] > 580 or ys[0] < 0 or ys[0] > 580: die(s, score)
+	if collide(xs[0], applepos[0], ys[0], applepos[1], 40, 20, 40, 20):
+		score+=1;xs.append(700);ys.append(700);applepos=(random.randint(0,590),random.randint(0,590))
+	if xs[0] < 0 or xs[0] > 580 or ys[0] < 0 or ys[0] > 580: die(s, score);break
 	i = len(xs)-1
 	while i >= 1:
 		xs[i] = xs[i-1];ys[i] = ys[i-1];i -= 1
