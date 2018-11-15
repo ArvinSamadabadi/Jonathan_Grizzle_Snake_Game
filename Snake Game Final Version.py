16f0cabd9b62034c0bbbb5abedf31b6d1494faec
jgrizz13 <44587555+jgrizz13@users.noreply.github.com>
2018-11-15 12:35:35 -0500

    Updated Display

--- Snake Game Final Version.py
+++ Snake Game Final Version.py
@@ -33,15 +33,22 @@ def die(screen, score):
 	screen.blit(t, (10, 270))
 	pygame.display.update()
 	pygame.time.wait(0000)
-xs = [290, 290, 290, 290, 290];ys = [290, 270, 250, 230, 210]
+xs = [290, 290, 290, 290, 290]
+ys = [290, 270, 250, 230, 210]
 dirs = 0
 score = 0
 applepos = (random.randint(0, 590), random.randint(0, 590))
-pygame.init();s=pygame.display.set_mode((600, 600))
+pygame.init()
+s=pygame.display.set_mode((600, 600))
+pygame.
 pygame.display.set_caption('Snake Game')
 Food = pygame.Surface((20,20));Food.fill((0, 0, 238))
 Snake = pygame.Surface((40, 40));Snake.fill((255,255,0))
 f = pygame.font.SysFont('Times New Roman', 20)
+t=f.render("Welcome to the snake game.", True, (255, 255, 255))
+s.blit(t, (10, 270))
+pygame.display.update()
+pygame.time.wait(10000)
 clock = pygame.time.Clock()
 
 while True:
@@ -61,21 +68,26 @@ while True:
 			break
 		i-= 1
 	if collide(xs[0], applepos[0], ys[0], applepos[1], 40, 20, 40, 20):
-		score+=1;xs.append(700);ys.append(700)
+		score+=1;xs.append(700)
+		ys.append(700)
 		applepos=(random.randint(0,590),random.randint(0,590))
 	if xs[0] < 0 or xs[0] > 580 or ys[0] < 0 or ys[0] > 580: die(s, score);break
 	i = len(xs)-1
 	while i >= 1:
-		xs[i] = xs[i-1];ys[i] = ys[i-1];i -= 1
+		xs[i] = xs[i-1]
+		ys[i] = ys[i-1]
+		i -= 1
 	if dirs==0:ys[0] += 20
 	elif dirs==1:xs[0] += 20
 	elif dirs==2:ys[0] -= 20
 	elif dirs==3:xs[0] -= 20	
 	s.fill((0,0,0))	
 	for i in range(0, len(xs)):
+		
 		s.blit(Snake, (xs[i], ys[i]))
 	s.blit(Food, applepos)
-	t=f.render(str(score), True, (255, 255, 0));s.blit(t, (10, 10))
+	t=f.render(str(score), True, (255, 255, 0))
+	s.blit(t, (10, 10))
 
 	pygame.display.update()
 					
