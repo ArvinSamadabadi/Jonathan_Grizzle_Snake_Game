b68a17dc1e6ae43e9be928ff3d03aae0ed86e7e4
Dominic Thomas <dominic.m.thomas@gmail.com>
2019-11-04 11:14:42 -0500

    separating collide code into its own file
    just a demo of code separation

--- Snake Game Final Working Version.py
+++ Snake Game Final Working Version.py
@@ -2,13 +2,9 @@ import pygame, random, sys
 from pygame.locals import *
 import time
 #import winsound
-
+import collide
       
-def collide(x1, x2, y1, y2, w1, w2, h1, h2):
-	if x1+w1>x2 and x1<x2+w2 and y1+h1>y2 and y1<y2+h2:
-		return True
-	else:
-		return False
+
 	
 def die(screen, score):
 	f=pygame.font.SysFont('Times New Roman', 30)
